18be9d6e3fed032b743ad22923725b8a4ef820ac
Diana Buras <burasdiana@hotmail.com>
2018-11-22 14:38:13 +0100

    [add] singleton class

---
+++ GameEngine/GameEngine/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    class Player
+    {
+        //create a Player object
+        private static Player _instance = new Player();
+
+        //private constructor so class cannot be instantiated
+        private Player() { }
+
+        //get the only Player object
+        public static Player getInstance()
+        {
+            return _instance;
+        }
+
+        public void DoStuff()
+        {
+            Console.WriteLine("Doing stuff...");
+        }
+    }
+}
