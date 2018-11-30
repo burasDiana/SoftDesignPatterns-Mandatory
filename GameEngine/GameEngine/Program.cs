82d4cba766dbd4a08fdd9856120b5b16daefdbb3
Diana Buras <burasdiana@hotmail.com>
2018-11-30 17:40:07 +0100

    [add] pair two arrays of elements with each other

--- GameEngine/GameEngine/Program.cs
+++ GameEngine/GameEngine/Program.cs
@@ -10,20 +10,49 @@ namespace GameEngine
     {
         static void Main(string[] args)
         {
-            //illegal construct
-            //Player player = new Player();
-            Game game = new Game()
+            int i = 0;
+            if (i > 0)
             {
-                Title = "Game1",
-                Genre = "Action-Adventure",
-                ReleaseDate = new DateTime(2013, 04, 28),
-            };
-            //get the only Player object
-            Player player = Player.getInstance();
-            player.DoStuff(game);
+                //illegal construct
+                //Player player = new Player();
+                Game game = new Game()
+                {
+                    Title = "Game1",
+                    Genre = "Action-Adventure",
+                    ReleaseDate = new DateTime(2013, 04, 28),
+                };
+                //get the only Player object
+                Player player = Player.getInstance();
+                player.DoStuff(game);
+
+                Console.ReadLine();
+            }
+
+            int[] depFlights = new[] {1, 2, 3, 4};
+            int[] arrFlights = new[] {10, 20, 30};
+
+            Console.WriteLine("The pairs are:" + '\n');
+
+            foreach (var pair in CreatePairs(depFlights,arrFlights))
+            {
+                Console.WriteLine(pair + '\n');
+            }
 
             Console.ReadLine();
         }
 
+        private static List<string> CreatePairs(int[] depFlights, int[]arrFlights)
+        {
+            List<string> results = new List<string>();
+            int i = 0;
+            foreach (var d in depFlights)
+            {
+                foreach (var a in arrFlights)
+                {
+                    results.Add(d + "-" + a);
+                }
+            }
+            return results;
+        }
     }
 }
