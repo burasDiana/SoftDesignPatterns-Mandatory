1460c18304aa19fe4e3d1ccd3d6b44c51682ac2d
Diana Buras <burasdiana@hotmail.com>
2018-12-07 16:27:03 +0100

    [add] adapter , advanced player interface and platform concrete classes

---
+++ GameEngine/GameEngine/AdvancedConsolePlayer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    interface AdvancedConsolePlayer
+    {
+        void PlayPS4(Game game);
+        void PlayNintendo(Game game);
+        void PlayXbox(Game game);
+    }
+
+    public class PS4Player : AdvancedConsolePlayer
+    {
+        public void PlayPS4(Game game)
+        {
+            Console.WriteLine("Playing " + game.Title + " on PS4.");
+        }
+        public void PlayNintendo(Game game)
+        {
+            
+        }
+
+        public void PlayXbox(Game game)
+        {
+            
+        }
+    }
+    public class NintendoPlayer : AdvancedConsolePlayer
+    {
+        public void PlayPS4(Game game)
+        {
+            
+        }
+        public void PlayNintendo(Game game)
+        {
+            Console.WriteLine("Playing " + game.Title + " on NintendoSwitch.");
+        }
+
+        public void PlayXbox(Game game)
+        {
+
+        }
+    }
+    public class XboxPlayer : AdvancedConsolePlayer
+    {
+        public void PlayPS4(Game game)
+        {
+
+        }
+
+        public void PlayNintendo(Game game)
+        {
+
+        }
+
+        public void PlayXbox(Game game)
+        {
+            Console.WriteLine("Playing " + game.Title + " on Xbox.");
+        }
+    }
+}
---
+++ GameEngine/GameEngine/ConsolePlayerAdapter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    class ConsolePlayerAdapter
+    {
+        private AdvancedConsolePlayer advancedConsolePlayer;
+
+        //constructor which instantiates the correct type class of advancedconsoleplayer
+        public ConsolePlayerAdapter(Game.GameTypes gameType)
+        {
+            if (gameType == Game.GameTypes.NintendoSwitch)
+            {
+                advancedConsolePlayer = new NintendoPlayer();
+            }
+            else if (gameType == Game.GameTypes.PS4)
+            {
+                advancedConsolePlayer = new PS4Player();
+            }
+            else if (gameType == Game.GameTypes.XBox)
+            {
+                advancedConsolePlayer = new XboxPlayer();
+            }
+        }
+
+        // play method of adapter which calls the playmethod of each type's class
+        public void Play(Game.GameTypes type, Game game)
+        {
+            if (type == Game.GameTypes.NintendoSwitch)
+            {
+                advancedConsolePlayer.PlayNintendo(game);
+            }
+            else if (type == Game.GameTypes.PS4)
+            {
+                advancedConsolePlayer.PlayPS4(game);
+            }
+            else if (type == Game.GameTypes.XBox)
+            {
+                advancedConsolePlayer.PlayXbox(game);
+            }
+        }
+    }
+}
