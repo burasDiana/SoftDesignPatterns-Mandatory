7930172291c146fe9f131d23821b8dad07400f9d
Diana Buras <burasdiana@hotmail.com>
2018-12-07 16:25:06 +0100

    [add] GameTypes enum to represent which platform the game is playable on

--- GameEngine/GameEngine/Game.cs
+++ GameEngine/GameEngine/Game.cs
@@ -9,23 +9,29 @@ namespace GameEngine
     public class Game
     {
         public string Title { get; set; } //generates auto-property
-        public enum GenreTypes { Adventure, Action, RolePlay, Simulation, Strategy };
-        private GenreTypes Genre { get; set; }
+        public enum GenreTypes { Undefined, Adventure, Action, RolePlay, Simulation, Strategy };
+        public enum GameTypes { Undefined , PC, NintendoSwitch, PS4, XBox};
+        private GenreTypes Genre { get; }
+        private GameTypes Type { get; }
 
         public GenreTypes Get_Genre()
         {
             return Genre;
         }
+        public GameTypes Get_Type()
+        {
+            return Type;
+        }
         public int ReleaseYear { get; set; }
         public double PopularityPercent { get; set; }
 
-        public Game(string title, GenreTypes genre, int releaseYear, double popularity)
+        public Game(string title, GenreTypes genre, GameTypes type, int releaseYear, double popularity)
         {
             Title = title;
             Genre = genre;
+            Type = type;
             ReleaseYear = releaseYear;
             PopularityPercent = popularity;
         }
-
     }
 }
