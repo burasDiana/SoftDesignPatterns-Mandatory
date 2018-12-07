64dc76db0142afd753e2fc928a8c9064003c96f0
Diana Buras <burasdiana@hotmail.com>
2018-12-07 16:28:15 +0100

    [update] main to use the adaptor pattern for playing a game

--- GameEngine/GameEngine/Program.cs
+++ GameEngine/GameEngine/Program.cs
@@ -12,30 +12,28 @@ namespace GameEngine
         {
             //illegal construct
             //Player player = new Player();
-            //Game game = new Game()
-            //{
-            //    Title = "Game1",
-            //    Genre = "Action-Adventure",
-            //};
-            ////get the only Player object
-            //Player playerMachine = Player.getInstance();
-            //playerMachine.PlayGame(game);
 
             DataSource data = new DataSource();
 
-            FilterCriteria adventure = new CriteriaAdventure();
-            FilterCriteria top5 = new CriteriaTop5();
-            FilterCriteria newest = new CriteriaThisYear();
-            FilterCriteria top5andnewest = new AndCriteria( top5,newest);
+            ////get the only Player object
+            Player playerMachine = Player.getInstance();
+            playerMachine.PlayGame(Game.GameTypes.PS4, data.GetGames().FirstOrDefault(g => g.Title =="Doom") );
 
-            Console.WriteLine("\nAdventure :");
-            PrintResults(adventure.MeetCriteria(data.GetGames()));
+            #region FilterPattern
+            //FilterCriteria adventure = new CriteriaAdventure();
+            //FilterCriteria top5 = new CriteriaTop5();
+            //FilterCriteria newest = new CriteriaThisYear();
+            //FilterCriteria top5andnewest = new AndCriteria( top5,newest);
 
-            Console.WriteLine("\nTop 5 :");
-            PrintResults(top5.MeetCriteria(data.GetGames()));
+            //Console.WriteLine("\nAdventure :");
+            //PrintResults(adventure.MeetCriteria(data.GetGames()));
 
-            Console.WriteLine("\nTop 5 and newest :");
-            PrintResults(top5andnewest.MeetCriteria(data.GetGames()));
+            //Console.WriteLine("\nTop 5 :");
+            //PrintResults(top5.MeetCriteria(data.GetGames()));
+
+            //Console.WriteLine("\nTop 5 and newest :");
+            //PrintResults(top5andnewest.MeetCriteria(data.GetGames())); 
+            #endregion
 
             Console.ReadLine();
         }
