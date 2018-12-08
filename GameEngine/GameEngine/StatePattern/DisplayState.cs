965f4eacd25a3bd1bf6a53cb1f75193fb3ceb211
Diana Buras <burasdiana@hotmail.com>
2018-12-08 15:22:22 +0100

    [add] display state concrete class

---
+++ GameEngine/GameEngine/StatePattern/DisplayState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.StatePattern
+{
+    class DisplayState : State
+    {
+        public void DoAction(PlayerMachine machine)
+        {
+            Console.WriteLine("Player machine is in display mode");
+            machine.SetState(this);
+        }
+    }
+}
