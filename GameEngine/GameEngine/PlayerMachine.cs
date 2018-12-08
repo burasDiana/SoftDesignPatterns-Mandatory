5ee7c5856c064eeb59cde900710e839b4195e514
Diana Buras <burasdiana@hotmail.com>
2018-12-08 15:23:14 +0100

    [add] modify PlayerMachine class to include state

--- GameEngine/GameEngine/PlayerMachine.cs
+++ GameEngine/GameEngine/PlayerMachine.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameEngine.StatePattern;
 
 namespace GameEngine
 {
@@ -11,13 +12,28 @@ namespace GameEngine
         //create a PlayerMachine object
         private static PlayerMachine _instance = new PlayerMachine();
 
+        private State _state;
+
         //private constructor so class cannot be instantiated
-        private PlayerMachine() { }
+        private PlayerMachine()
+        {
+            this._state = null;
+        }
 
         //get the only PlayerMachine object
         public static PlayerMachine getInstance()
         {
             return _instance;
+        }
+
+        public void SetState(State state)
+        {
+            this._state = state;
+        }
+
+        public State GetState()
+        {
+            return _state;
         }
 
         //instance of adapter class
