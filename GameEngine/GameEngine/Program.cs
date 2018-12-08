35cbf98f9d38ec461e05e41e8929b7c54ee59614
Diana Buras <burasdiana@hotmail.com>
2018-12-08 14:44:05 +0100

    [add] add client code for decorator pattern

--- GameEngine/GameEngine/Program.cs
+++ GameEngine/GameEngine/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameEngine.DecoratorPattern;
 
 namespace GameEngine
 {
@@ -11,19 +12,22 @@ namespace GameEngine
         static void Main(string[] args)
         {
             //illegal construct
-            //Player player = new Player();
+            //PlayerMachine player = new PlayerMachine();
 
+            //games data set
             DataSource data = new DataSource();
 
-            ////get the only Player object
-            Player playerMachine = Player.getInstance();
-            playerMachine.PlayGame(Game.GameTypes.PS4, data.GetGames().FirstOrDefault(g => g.Title =="Doom") );
+            ////get the only PlayerMachine object
+            PlayerMachine playerMachine = PlayerMachine.getInstance();
+
+            //AdapterPattern
+            //playerMachine.PlayGame(Game.GameTypes.PS4, data.GetGames().FirstOrDefault(g => g.Title =="Doom") );
 
             #region FilterPattern
             //FilterCriteria adventure = new CriteriaAdventure();
             //FilterCriteria top5 = new CriteriaTop5();
             //FilterCriteria newest = new CriteriaThisYear();
-            //FilterCriteria top5andnewest = new AndCriteria( top5,newest);
+            //FilterCriteria top5andnewest = new AndCriteria(top5, newest);
 
             //Console.WriteLine("\nAdventure :");
             //PrintResults(adventure.MeetCriteria(data.GetGames()));
@@ -32,7 +36,21 @@ namespace GameEngine
             //PrintResults(top5.MeetCriteria(data.GetGames()));
 
             //Console.WriteLine("\nTop 5 and newest :");
-            //PrintResults(top5andnewest.MeetCriteria(data.GetGames())); 
+            //PrintResults(top5andnewest.MeetCriteria(data.GetGames()));
+            #endregion
+
+            #region DecoratorPattern
+
+            ////get the game for which we want to play a DLC
+            //Game originalGame = data.GetGames().FirstOrDefault(g => g.Title == "Fallout");
+
+            ////create the dlc linked to the originalGame
+            //Game_DLC fallout_DLC = new Game_DLC(originalGame.Title, "NewVegas");
+
+            ////Make the game playable
+            //PlayableDLC playableDLC = new PlayableDLC(fallout_DLC);
+
+            //playableDLC.Play_Dlc();
             #endregion
 
             Console.ReadLine();
