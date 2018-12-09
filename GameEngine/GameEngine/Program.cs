8d67c447489a6752bebf6198b35d3f76578fe75b
Diana Buras <burasdiana@hotmail.com>
2018-12-09 18:41:39 +0100

    [add] client functionality for displaying games, filtering games and playing games

--- GameEngine/GameEngine/Program.cs
+++ GameEngine/GameEngine/Program.cs
@@ -14,47 +14,140 @@ namespace GameEngine
         {
             //illegal construct
             //PlayerMachine player = new PlayerMachine();
-
+            bool flag = true;
             //games data set
             DataSource data = new DataSource();
 
-            ////get the only PlayerMachine object
+            //get the only PlayerMachine object
+            Console.WriteLine("Performing machine setup...");
             PlayerMachine playerMachine = PlayerMachine.getInstance();
 
-            #region StatePattern
-            //DisplayState displayMode = new DisplayState();
-            //PlayState playMode = new PlayState();
-            //StandbyState standby = new StandbyState();
+            //state pattern
+            DisplayState displayMode = new DisplayState();
+            PlayState playMode = new PlayState();
+            StandbyState standby = new StandbyState();
 
-            //Get_Machine_Current_State(playerMachine);
+            Console.WriteLine("Setup complete. Current mode: ");
+            Get_Machine_Current_State(playerMachine);
 
-            //displayMode.DoAction(playerMachine);
+            while (flag)
+            {
+                Console.WriteLine("You now have the following options: \n 1 - Display games \n 2 - Display filters");
 
-            //Get_Machine_Current_State(playerMachine);
+                //display games
+                bool inputValid = true;
+                bool canPlayGame = false;
+                int input;
+                input = GetInput();
+                displayMode.DoAction(playerMachine);
 
-            //playMode.DoAction(playerMachine);
+                if (input == 1)
+                {
+                    PrintResults(data.GetGames());
+                    canPlayGame = true;
+                }
+                else if (input == 2)
+                {
+                    Console.WriteLine("You can chose one of the following filters : \n Filter by game types :\n 1 - Adventure \n 2 - Action \n 3 - RolePlay \n Filter by platform :\n 4 - PS4 \n 5 - Nintendo  \n 6 - Xbox  \n 7 - PC  \n Other filters :  \n 8 - 90's Games \n 9 - This year  \n 10 - Top 5 rated \n \n You can also combine filters by using 'and' and 'or'. \n Examples: 3and10 ; 5or6 ; 2 ");
 
-            //Get_Machine_Current_State(playerMachine); 
-            #endregion
+                    var input2 = Console.ReadLine();
+                    FilterCriteria finalCriteria = new CriteriaAdventure();
+
+                    if (IsInputString(input2))
+                    {
+                        //display games filtered by 2 criteria
+                        if (input2.Contains("and"))
+                        {
+                            var splitinput = input2.Split(new[] { "and" }, StringSplitOptions.None);
+
+                            int id1 = Int32.Parse(splitinput[0]);
+                            int id2 = Int32.Parse(splitinput[1]);
+
+                            if (IsIdValid(id1) && IsIdValid(id2))
+                            {
+                                FilterCriteria criteria1 = GetFilterCriteria(id1);
+                                FilterCriteria criteria2 = GetFilterCriteria(id2);
+                                finalCriteria = new AndCriteria(criteria1, criteria2);
+                            }
+                            else
+                            {
+                                inputValid = false;
+                                Console.WriteLine("Input invalid, please retry.");
+                            }
+                            
+                        }
+                        else if (input2.Contains("or"))
+                        {
+                            var splitinput = input2.Split(new[] { "or" }, StringSplitOptions.None);
+
+                            int id1 = Int32.Parse(splitinput[0]);
+                            int id2 = Int32.Parse(splitinput[1]);
+
+                            if (IsIdValid(id1) && IsIdValid(id2))
+                            {
+                                FilterCriteria criteria1 = GetFilterCriteria(id1);
+                                FilterCriteria criteria2 = GetFilterCriteria(id2);
+                                finalCriteria = new OrCriteria(criteria1, criteria2);
+                            }
+                            else
+                            {
+                                inputValid = false;
+                                Console.WriteLine("Input invalid, please retry.");
+                            }
+                        } 
+                    }
+                    else
+                    {
+                        //display games filtered by 1 criteria
+                        int id = Int32.Parse(input2);
+
+                        if (IsIdValid(id))
+                        {
+                            finalCriteria = GetFilterCriteria(id);
+                        }
+                        else
+                        {
+                            inputValid = false;
+                            Console.WriteLine("Input invalid, please retry.");
+                        }
+                    }
+
+                    if (inputValid) //display filter criterias only if they are valid
+                    {
+                        Console.WriteLine(finalCriteria + " Games :");
+                        PrintResults(finalCriteria.MeetCriteria(data.GetGames()));
+                        canPlayGame = true;
+                        //flag = false;
+                    }
+                }
+                else //input was not 2 or 1
+                {
+                    Console.WriteLine("Input invalid, please retry.");
+                }
+
+                if (canPlayGame)
+                {
+                    //play a game 
+                    Console.WriteLine("Write the id of the game you wish to play: ");
+                    input = GetInput();
+                    if (input <= 0 || input > 11)
+                    {
+                        Console.WriteLine("Input invalid, please retry.");
+                    }
+                    else
+                    {
+                        playMode.DoAction(playerMachine);
+                        playerMachine.PlayGame(data.GetGames().FirstOrDefault(g => g.Id == input));
+                    }
+
+                    Console.WriteLine("\n \nPress enter to go back to display mode.");
+                    Console.ReadLine();
+                }
+
+            }
 
             #region AdapterPattern
             //playerMachine.PlayGame(data.GetGames().FirstOrDefault(g => g.Title =="Doom") ); 
-            #endregion
-
-            #region FilterPattern
-            //FilterCriteria adventure = new CriteriaAdventure();
-            //FilterCriteria top5 = new CriteriaTop5();
-            //FilterCriteria newest = new CriteriaThisYear();
-            //FilterCriteria top5andnewest = new AndCriteria(top5, newest);
-
-            //Console.WriteLine("\nAdventure :");
-            //PrintResults(adventure.MeetCriteria(data.GetGames()));
-
-            //Console.WriteLine("\nTop 5 :");
-            //PrintResults(top5.MeetCriteria(data.GetGames()));
-
-            //Console.WriteLine("\nTop 5 and newest :");
-            //PrintResults(top5andnewest.MeetCriteria(data.GetGames()));
             #endregion
 
             #region DecoratorPattern
@@ -71,7 +164,78 @@ namespace GameEngine
             //playableDLC.Play_Dlc();
             #endregion
 
+            Console.WriteLine("\n Press any key to exit.");
             Console.ReadLine();
+        }
+
+        //filter pattern
+        public static FilterCriteria GetFilterCriteria(int id)
+        {
+            FilterCriteria newCriteria = new CriteriaAdventure();
+            switch (id)
+            {
+                case 1:
+                    newCriteria = new CriteriaAdventure();
+                    break;
+                case 2:
+                    newCriteria = new CriteriaAction();
+                    break;
+                case 3:
+                    newCriteria = new CriteriaRolePlay();
+                    break;
+                case 4:
+                    newCriteria = new CriteriaPS4();
+                    break;
+                case 5:
+                    newCriteria = new CriteriaNintendo();
+                    break;
+                case 6:
+                    newCriteria = new CriteriaXbox();
+                    break;
+                case 7:
+                    newCriteria = new CriteriaPC();
+                    break;
+                case 8:
+                    newCriteria = new Criteria90S();
+                    break;
+                case 9:
+                    newCriteria = new CriteriaThisYear();
+                    break;
+                case 10:
+                    newCriteria = new CriteriaTop5();
+                    break;
+            }
+            return newCriteria;
+        }
+
+        private static bool IsInputString(string input)
+        {
+            int val;
+            if (Int32.TryParse(input, out val))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static int GetInput()
+        {
+            int a;
+            if (Int32.TryParse(Console.ReadLine(), out a))
+            {
+                return a;
+            }
+            return -1;
+        }
+
+        public static bool IsIdValid(int id)
+        {
+            if (id > 0 && id < 12)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public static void Get_Machine_Current_State(PlayerMachine playerMachine)
@@ -82,7 +246,7 @@ namespace GameEngine
         {
             foreach (var game in filteredGames)
             {
-                Console.WriteLine("Game : [Title: " + game.Title + ", Genre: "+ game.Get_Genre() + ", Released: " + game.ReleaseYear + ", Score :" + game.PopularityPercent + "% ]");
+                Console.WriteLine("Game : [Id: " + game.Id + ", Title: " + game.Title +  ", Genre: " + game.Get_Genre() + ", Platform: " + game.Get_Type() + ", Released: " + game.ReleaseYear + ", Rating :" + game.PopularityRating + "% ]");
             }
         }
     }
