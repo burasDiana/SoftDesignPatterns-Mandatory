7c0f6481a42f23cf1cc783fb1bbeab7bc8463ba1
Diana Buras <burasdiana@hotmail.com>
2019-02-13 20:59:15 +0100

    [add] comment state

--- GameEngine/GameEngine/StatePattern/State.cs
+++ GameEngine/GameEngine/StatePattern/State.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace GameEngine.StatePattern
 {
+    //this class represents the interface implemented by the different state objects
     interface State
     {
         void DoAction(PlayerMachine machine);
