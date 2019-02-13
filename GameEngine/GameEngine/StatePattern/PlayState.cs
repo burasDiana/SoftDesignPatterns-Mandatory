d72bbf2c42cb4d60ea9b146b170fcfbda06846f1
Diana Buras <burasdiana@hotmail.com>
2019-02-13 20:33:54 +0100

    [add] comment play state

--- GameEngine/GameEngine/StatePattern/PlayState.cs
+++ GameEngine/GameEngine/StatePattern/PlayState.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace GameEngine.StatePattern
 {
+    //this class represents the play state of the machine
     class PlayState : State
     {
         public void DoAction(PlayerMachine machine)
